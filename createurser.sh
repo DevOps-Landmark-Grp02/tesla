034f807ba4cd241ae8dcc651026e04937d27e3cf
Dorinetk <dketchiozo@gmail.com>
2022-11-26 12:33:15 -0500

    create user file

---
+++ createurser.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Creating a user
+echo "Please enter the user name for the account you want to create!"
+read userName
+echo "The name you entered is: "  $userName
+sudo /usr/sbin/useradd   $userName
+sudo groupadd devops
+sudo /usr/sbin/usermod -G devops $userName
+echo ".......User is created..........."
+echo ".......Now Set the password for  ....... $userName"
+sudo passwd $userName
